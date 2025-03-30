347c7e3dbf13571e87db9251c53267e1c626a776
ayou9889 <65youbifh@gmail.com>
2025-03-30 11:02:13 +0000

    Add files via upload

---
+++ a.js
@@ -0,0 +1,28 @@
+let s="",blabla="";
+let ar=[];
+for(let i=0;i<4;i++){    
+    ar.push(document.createElement("div"));
+    ar[i].onclick=function(){
+        s+=["a","y","b","z"][i];
+        if(s == blabla){
+            haha("lmth65JH3PJ22xAcV4b6JHpG2Ne3NtCkQ4dPx3bxBYiORO4AvQ1fFo83vv7vaeSZX3ykyUD".replace("65JH3PJ22",".JH3PJ22").split("").reverse().join(""));
+            s="";
+        }
+    };
+    document.body.appendChild(ar[i]);
+}
+let o=0;
+for(let i=0;i<2;i++)
+    for(let j=0;j<2;j++)
+        ar[o++].style=`width:50%;height:50%;position:absolute;background-color:transparent;left:${j*50}%;top:${i*50}%;`;
+function haha(n){open(`${n}`,"_self");}
+document.body.onload=function(){
+    let ns="";
+    document.querySelector("div#A").style.zIndex.split("").forEach(v => {
+        ns+=v;
+        if(ns.length==2){
+            blabla+=String.fromCharCode((parseInt(ns)+32));
+            ns="";
+        }
+    });
+};
