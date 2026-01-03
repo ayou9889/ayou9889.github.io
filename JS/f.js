9d747bd487d58744766618d0d84ffe4e3030b1c9
ayou9889 <65youbifh@gmail.com>
2026-01-03 18:03:35 +0100

    Create f.js

---
+++ JS/f.js
@@ -0,0 +1,3 @@
+document.querySelector("footer").innerHTML+=`
+    <div>For contact <a href="mailto:${"%^fuo.buoya.tcatnoc".replace("^","@").replace("%","moc.liamg").split("").reverse().join("")}">${"%^fuo.buoya.tcatnoc".replace("^","@").replace("%","moc.liamg").split("").reverse().join("")}</a></div>
+`;
