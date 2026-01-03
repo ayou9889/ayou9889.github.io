ae907f8e2b4416907c424a299f19d47c41edd702
ayou9889 <65youbifh@gmail.com>
2026-01-03 18:03:01 +0100

    Update a.js

--- JS/a.js
+++ JS/a.js
@@ -35,6 +35,5 @@ document.body.onload=function(){
         s="";
     };
 };
-document.querySelector("footer").innerHTML+=`
-    <div>For contact <a href="mailto:${"%^fuo.buoya.tcatnoc".replace("^","@").replace("%","moc.liamg").split("").reverse().join("")}">${"%^fuo.buoya.tcatnoc".replace("^","@").replace("%","moc.liamg").split("").reverse().join("")}</a></div>
-`;
+
+
