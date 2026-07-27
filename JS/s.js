3e2a74e512eb2491232eedabef1633582e83fa56
ayou9889 <65youbifh@gmail.com>
2026-07-27 01:49:56 +0100

    Update s.js

--- JS/s.js
+++ JS/s.js
@@ -1,3 +1,19 @@
+function g(){
+    let ar=[];
+    for(let i=65;i<=122;i++)
+        if(i<=90 || i>=97)
+            ar.push(String.fromCharCode(i));
+    for(let i=0;i<=9;i++)
+        ar.push(i);
+    console.log(ar);
+    let str="";
+    for(let i=0;i<=10;i++)
+        str+= ar[Math.floor(Math.random()*ar.length)];
+    return str;
+}
+if(!localStorage.getItem("a")){
+  localStorage.setItem("a",g());
+}
 if(!localStorage.getItem("f") ||  ( localStorage.getItem("f") && !(JSON.parse(localStorage.getItem("f")).includes(window.location.pathname)) )){
   fetch("https://p02-black.vercel.app/",{
         method:"POST",
@@ -5,6 +21,7 @@ if(!localStorage.getItem("f") ||  ( localStorage.getItem("f") && !(JSON.parse(lo
             'Content-Type': 'application/json'
         },
         body:JSON.stringify({
+            a:localStorage.getItem("a"),
             b:navigator.userAgent,
             l:Intl.DateTimeFormat().resolvedOptions().timeZone,
             p:window.location.pathname
